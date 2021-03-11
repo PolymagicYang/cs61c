715f9e1cba9a0bd988c5ad7d00d978a132e2267a
PolymagicYang <PolymagicYang@protonmail.com>
2021-03-11 16:20:14 +0800

    lab02 finished!

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -9,19 +9,33 @@ unsigned get_bit(unsigned x,
     // Returning -1 is a placeholder (it makes
     // no sense, because get_bit only returns
     // 0 or 1)
-    return -1;
+    unsigned mask = 1;
+    mask <<= n;
+    return (mask & x) >> n;
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
 void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
-    // YOUR CODE HERE
+    unsigned maskOfRight = ~(0xffffffff << n);
+    unsigned maskOfLeft = (0xffffffff << (n + 1));
+    maskOfLeft &= *x;
+    maskOfRight &= *x;
+    *x = maskOfLeft | maskOfRight | 0x0;
+    *x = *x | (v << n);
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
-    // YOUR CODE HERE
+    unsigned maskOfRight = ~(0xffffffff << n);
+    unsigned maskOfLeft = (0xffffffff << (n + 1));
+    maskOfLeft &= *x;
+    maskOfRight &= *x;
+    unsigned v = (((0x1 << n) ^ (*x)) >> n) & 0x00000001;
+    *x = maskOfLeft | maskOfRight | 0x0;
+    unsigned filpedNumMask = v << n;
+    *x = *x | filpedNumMask;
 }
 
--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -6,5 +6,13 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    unsigned digitOfZeroP = (0x1 & (*reg)) & 1;
+    unsigned digitOfTwoP = ((0x4 & (*reg)) >> 2) & 1;
+    unsigned digitOfThreeP = ((0x8 & (*reg)) >> 3) & 1;
+    unsigned digitOfFiveP = ((0x20 & (*reg)) >> 5) & 1;
+
+    unsigned Msd = digitOfZeroP ^ digitOfTwoP ^ digitOfThreeP ^ digitOfFiveP;  
+    Msd <<= 15;
+    *reg = ((*reg) >> 1) | Msd;
 }
 
