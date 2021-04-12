fb0879d77ce2552d5e416e382339794d32913e4e
PolymagicYang <PolymagicYang@protonmail.com>
2021-04-12 17:48:41 +0800

    lab09 finished

--- lab09/common.c
+++ lab09/common.c
@@ -50,13 +50,38 @@ long long int sum_simd(unsigned int vals[NUM_ELEMS]) {
 	__m128i _127 = _mm_set1_epi32(127);		// This is a vector with 127s in it... Why might you need this?
 	long long int result = 0;				   // This is where you should put your final result!
 	/* DO NOT DO NOT DO NOT DO NOT WRITE ANYTHING ABOVE THIS LINE. */
+	int sum;
+	int remaining_sum;
+	__m128i temp_sum;  
+	int temp_result[4] = {0, 0, 0, 0};
 	
 	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
 		/* YOUR CODE GOES HERE */
+		sum = 0;
+		remaining_sum = 0;
+		temp_sum = _mm_setzero_si128();
+
+		for (unsigned int i = 0; i < NUM_ELEMS / 4 * 4; i += 4) {
+			__m128i temp = _mm_loadu_si128((__m128i *) &vals[i]);
+		    __m128i isGreaterArray = _mm_cmpgt_epi32(temp, _127);
+			temp = _mm_and_si128(temp, isGreaterArray);
+			temp_sum = _mm_add_epi32(temp_sum, temp);
+		}
+		_mm_storeu_si128((__m128i *) &temp_result[0], temp_sum);
 
 		/* You'll need a tail case. */
+		for (unsigned int index = NUM_ELEMS / 4 * 4; index < NUM_ELEMS; index++) {
+			remaining_sum += vals[index];
+		}
 
+		for (unsigned int i = 0; i < 4; i++) {
+			sum += temp_result[i];
+		}
+		sum += remaining_sum;
+
+		result += sum;
 	}
+
 	clock_t end = clock();
 	printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
 	return result;
@@ -66,12 +91,59 @@ long long int sum_simd_unrolled(unsigned int vals[NUM_ELEMS]) {
 	clock_t start = clock();
 	__m128i _127 = _mm_set1_epi32(127);
 	long long int result = 0;
+	unsigned int sum = 0;
+	unsigned int remaining_sum;
+	__m128i temp_sum;
+	int temp_result[4] = {0, 0, 0, 0};
+
 	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
 		/* COPY AND PASTE YOUR sum_simd() HERE */
-		/* MODIFY IT BY UNROLLING IT */
+		/* YOUR CODE GOES HERE */
+		sum = 0;
+		remaining_sum = 0;
+		temp_sum = _mm_setzero_si128();
 
-		/* You'll need 1 or maybe 2 tail cases here. */
+		for (unsigned int i = 0; i < NUM_ELEMS / 16 * 16; i += 16) {
+			__m128i temp1 = _mm_loadu_si128((__m128i *) &vals[i]);
+		    __m128i isGreaterArray1 = _mm_cmpgt_epi32(temp1, _127);
+			temp1 = _mm_and_si128(temp1, isGreaterArray1);
 
+			__m128i temp2 = _mm_loadu_si128((__m128i *) &vals[i + 4]);
+		    __m128i isGreaterArray2 = _mm_cmpgt_epi32(temp2, _127);
+			temp2 = _mm_and_si128(temp2, isGreaterArray2);
+			
+			__m128i temp3 = _mm_loadu_si128((__m128i *) &vals[i + 8]);
+		    __m128i isGreaterArray3 = _mm_cmpgt_epi32(temp3, _127);
+			temp3 = _mm_and_si128(temp3, isGreaterArray3);
+
+			__m128i temp4 = _mm_loadu_si128((__m128i *) &vals[i + 12]);
+		    __m128i isGreaterArray4 = _mm_cmpgt_epi32(temp4, _127);
+			temp4 = _mm_and_si128(temp4, isGreaterArray4);
+
+			temp_sum = _mm_add_epi32(temp_sum, temp1);
+			temp_sum = _mm_add_epi32(temp_sum, temp2);
+			temp_sum = _mm_add_epi32(temp_sum, temp3);
+			temp_sum = _mm_add_epi32(temp_sum, temp4);
+		}
+		
+		for (unsigned int i = NUM_ELEMS / 16 * 16; i < NUM_ELEMS / 4 * 4; i += 4) {
+			__m128i temp = _mm_loadu_si128((__m128i *) &vals[i]);
+		    __m128i isGreaterArray = _mm_cmpgt_epi32(temp, _127);
+			temp = _mm_and_si128(temp, isGreaterArray);
+			temp_sum = _mm_add_epi32(temp_sum, temp);
+		}
+		_mm_storeu_si128((__m128i *) &temp_result[0], temp_sum);
+		for (unsigned int i = 0; i < 4; i++) {
+			sum += temp_result[i];
+		}
+
+		/* You'll need a tail case. */
+		for (unsigned int index = NUM_ELEMS / 4 * 4; index < NUM_ELEMS; index++) {
+			remaining_sum += vals[index];
+		}
+
+		sum += remaining_sum;
+		result += sum;
 	}
 	clock_t end = clock();
 	printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
